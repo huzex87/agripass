6ae8b9f1cece644038eb6de8c7c13a6960cc5a7a
Huzex <huzex87@gmail.com>
2026-07-03 18:37:41 +0100

    config: update CORS configuration to allow Vercel origins dynamically

--- server/server.js
+++ server/server.js
@@ -14,11 +14,13 @@ app.use(cookieParser());
 connectDB();
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "http://localhost:3002",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || origin.startsWith("http://localhost:") || origin.endsWith(".vercel.app")) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: [
